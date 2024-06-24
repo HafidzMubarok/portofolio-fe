70a9eef53276ca88c287472706a063f9455568fe
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-06-24 21:22:27 +0700

    :hammer: Customize the input component

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -3,12 +3,24 @@
         <span class="font-semibold">
             <slot></slot>
         </span>
-        <input type="text"
-            class="rounded-lg p-3 border-0 outline-0 focus:ring-2 focus:ring-secondary ring-inset w-full">
+        <input 
+            type="text"
+            :id="props.inputId"
+            v-model="props.inputModel"
+            @input="emit('update:inputModel', $event.target.value)"
+            class="rounded-lg p-3 border-0 outline-0 focus:ring-2 focus:ring-secondary ring-inset w-full text-primary"
+        >
     </div>
 </template>
 
 <script setup>
+
+const props = defineProps({
+    inputModel: String,
+    inputId: String,
+});
+
+const emit = defineEmits(['update:inputModel'])
 
 </script>
 
