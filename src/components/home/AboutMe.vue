f69eb62874c8c1a2f1aa45aaf2e42d585e140e05
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-06-24 10:12:36 +0700

    :alien: Consume profile API

--- src/components/home/AboutMe.vue
+++ src/components/home/AboutMe.vue
@@ -2,7 +2,10 @@
     <section id="about-me" class="bg-primary scroll-mt-8 snap-start h-screen lg:px-24 lg:py-8">
         <div class="flex flex-col h-full justify-center px-10 py-8 space-y-4">
             <h1 class="text-light font-bold text-2xl lg:text-3xl">About Me</h1>
-            <p class="lg:text-lg text-light">
+            <p v-if="profile.about_me" class="lg:text-lg text-light">
+                {{ profile.about_me }}
+            </p>
+            <p v-else class="lg:text-lg text-light">
                 As a graduate of <strong>Bandung State Polytechnic with a Bachelor's degree in Computer Applied Sciences</strong>, I have immersed myself in an academic environment that demands rapid and continuous learning. This experience has shaped my ability to adapt and grow, both in team settings and independently.
                 <br><br>
                 I have a strong interest in <strong>software development</strong>, particularly in <strong>web development</strong>. My belief in continuous learning and technology exploration has guided me to engage in various software development projects, both professionally and personally.
@@ -12,7 +15,29 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+import axios from 'axios';
 
+const profile = ref([]);
+
+const fetchProfiles = async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/api/profile');
+
+        if (response && response.data) {
+            profile.value = response.data[0];
+            console.log(profile.value);
+        } else {
+            throw new Error('Struktur data tidak sesuai');
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+onMounted(() => {
+    fetchProfiles();
+});
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -4,8 +4,8 @@
             <div class="flex flex-wrap justify-end space-y-6">
                 <div class="lg:basis-1/2 self-center space-y-6">
                     <div class="w-full self-center space-y-6 md:w-auto">
-                        <h1 class="block font-bold text-light text-center text-4xl md:text-6xl lg:text-start">MUHAMMAD FATHUL HAFIDZ MUBAROK</h1>
-                        <h2 class="font-normal text-light text-center lg:text-start md:text-2xl">Software Engineer | Web Developer</h2>
+                        <h1 class="block font-bold uppercase text-light text-center text-4xl md:text-6xl lg:text-start">{{ profile.first_name }}  {{ profile.last_name }}</h1>
+                        <h2 class="font-normal text-light text-center lg:text-start md:text-2xl">{{ profile.professional_motto }}</h2>
                     </div>
                     <div class="w-full self-center space-y-6 md:w-auto">
                         <div class="text-center py-3 lg:text-start">
@@ -19,19 +19,19 @@
                 <div class="space-y-6 lg:mx-auto lg:pb-4 lg:basis-1/2">
                     <img src="/img/profile-pic-2.png" alt="Profile Pic" class="max-w-full mx-auto px-16 md:max-w-96 md:ps-0 lg:px-8">
                     <div class="space-x-6 text-light text-center">
-                            <a href="#">
+                            <a v-if="profile.email" href="#">
                                 <i class="fa-solid fa-envelope fa-xl"></i>
                             </a>
-                            <a href="#">
+                            <a v-if="profile.whatsapp" href="#">
                                 <i class="fa-brands fa-whatsapp fa-xl"></i>
                             </a>
-                            <a href="#">
+                            <a v-if="profile.linkedin" :href="profile.linkedin" target="_blank">
                                 <i class="fa-brands fa-linkedin fa-xl"></i>
                             </a>
-                            <a href="#">
+                            <a v-if="profile.github" :href="profile.github" target="_blank">
                                 <i class="fa-brands fa-github fa-xl"></i>
                             </a>
-                            <a href="#">
+                            <a v-if="profile.instagram" :href="profile.instagram" target="_blank">
                                 <i class="fa-brands fa-instagram fa-xl"></i>
                             </a>
                         </div>
@@ -42,5 +42,28 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+import axios from 'axios';
+
+const profile = ref([]);
+
+const fetchProfiles = async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/api/profile');
+
+        if (response && response.data) {
+            profile.value = response.data[0];
+            console.log(profile.value);
+        } else {
+            throw new Error('Struktur data tidak sesuai');
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+onMounted(() => {
+    fetchProfiles();
+});
 
 </script>
