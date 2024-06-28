112fe87a79f08f602067ac51ba70f18240c8a0cb
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-06-28 16:09:03 +0700

    :lipstick: Customize the style for the selected navigation item

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="sticky top-0 w-full bg-gradient-to-r from-sky-600 to-primary z-10">
     <nav>
-      <div class="flex flex-col font-semibold space-y-4 text-light text-2xl py-3 md:justify-center md:text-base md:px-6">
+      <div :class="{ 'md:pt-8': 'home' == props.section.currentSection }" class="flex flex-col font-semibold space-y-4 text-light text-2xl py-3 md:py-0 md:justify-center md:text-base md:px-6">
         <button data-collapse-toggle="navbar-default" type="button" class="px-6 text-end sm:flex sm:flex-row sm:justify-end md:hidden" aria-controls="navbar-default" aria-expanded="false">
           <i class="fa-solid fa-bars"></i>
         </button>
         <div class="hidden md:contents uppercase" id="navbar-default">
-          <ul class="flex flex-col w-full space-y-8 py-8 absolute text-center bg-primary md:flex md:flex-row md:justify-center md:sticky md:items-center md:space-x-8 md:space-y-0 md:py-0 md:bg-inherit lg:space-x-16">
-            <li><a href="#about-me" :class="{ active: 'about-me' == props.section.currentSection }">About Me</a></li>
-            <li><a href="#history" :class="{ active: 'history' == props.section.currentSection }">History</a></li>
-            <li><a href="#technology" :class="{ active: 'technology' == props.section.currentSection }">Technology</a></li>
-            <li><a href="#education" :class="{ active: 'education' == props.section.currentSection }">Education</a></li>
-            <li><a href="#article" :class="{ active: 'article' == props.section.currentSection }">Article</a></li>
-            <li><a href="#feedback" class="inline-block rounded bg-light px-6 pb-2 pt-2.5 w-max leading-normal
+          <ul class="flex flex-col w-full py-8 absolute text-center bg-primary md:flex md:flex-row md:justify-center md:align-middle md:sticky md:items-center md:space-x-4 md:pt-1 md:pb-0 md:bg-inherit lg:space-x-8">
+            <li class="py-6 md:pb-4 md:pt-3 md:px-4 lg:px-6" :class="{ 'border-s-4 border-s-secondary md:border-s-0 md:border-b-4 md:border-b-secondary font-bold active': 'about-me' == props.section.currentSection }">
+              <a href="#about-me">
+                About Me
+              </a>
+            </li>
+            <li class="py-6 md:pb-4 md:pt-3 md:px-4 lg:px-6" :class="{ 'border-s-4 border-s-secondary md:border-s-0 md:border-b-4 md:border-b-secondary font-bold active': 'history' == props.section.currentSection }">
+              <a href="#history">
+                History
+              </a>
+            </li>
+            <li class="py-6 md:pb-4 md:pt-3 md:px-4 lg:px-6" :class="{ 'border-s-4 border-s-secondary md:border-s-0 md:border-b-4 md:border-b-secondary font-bold active': 'technology' == props.section.currentSection }">
+              <a href="#technology">
+                Technology
+              </a>
+            </li>
+            <li class="py-6 md:pb-4 md:pt-3 md:px-4 lg:px-6" :class="{ 'border-s-4 border-s-secondary md:border-s-0 md:border-b-4 md:border-b-secondary font-bold active': 'education' == props.section.currentSection }">
+              <a href="#education">
+                Education
+              </a>
+            </li>
+            <li class="py-6 md:pb-4 md:pt-3 md:px-4 lg:px-6" :class="{ 'border-s-4 border-s-secondary md:border-s-0 md:border-b-4 md:border-b-secondary font-bold active': 'article' == props.section.currentSection }">
+              <a href="#article">
+                Article
+              </a>
+            </li>
+            <li class="py-6 md:py-0"><a href="#feedback" class="inline-block rounded bg-light px-6 pb-2 pt-2.5 w-max leading-normal
                   text-primary shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2
                   focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600
                   active:shadow-primary-2" data-twe-ripple-init data-twe-ripple-color="light">
@@ -41,11 +61,10 @@ onMounted(() => {
 
 <style scoped>
 .active{
-  font-weight: bold;
-  border-bottom: 2px solid #f1f5f9;
-  /* background-color: #f1f5f9;
-  color: #172554;
-  padding: 8px 24px 8px 24px;
-  border-radius: 4px; */
+  background-color: rgba(241,245,249,0.38);
+}
+
+.hero{
+  padding-top: 40px;
 }
 </style>
