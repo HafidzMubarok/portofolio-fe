e12bcc08414cdffde52f405954750ae69d5f1a86
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-07-04 14:09:50 +0700

    :zap: Adding vite plugin chunk split

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,13 @@ export default defineConfig({
     vue(),
     vueJsx(),
     VueDevTools(),
+    chunkSplitPlugin({
+      strategy: 'unbundle',
+      customSplitting: {
+        // All files in `src/container` will be merged together in `container` chunk
+        'container': [/src\/container/]
+      }
+    }),
     ViteImageOptimizer({
       jpg: {
         quality: 80,
